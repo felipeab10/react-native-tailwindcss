01be167c5eaf935fad0699ff987766eafc3ffbed
Felipe Almeida Batista <felipeab10@hotmail.com>
2022-12-02 19:36:43 -0400

    Mudança de estrutura de projeto New - Adicionado Autenticação no Backend New - Adicioando pacote para trabalhar com .env no frontend

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/auth/login', [AuthController::class,'login']);
+Route::group([
+    'middleware' => 'apiJwt',
+    'prefix' => 'auth',
+    'except' => 'login'
+], function ($router) {
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('me', [AuthController::class,'me']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
